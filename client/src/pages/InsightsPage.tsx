d7677a2b276ed2bc1f43b92c777a80f45a7daf91
Chaitanya Giri <chaitanyagiri2@gmail.com>
2024-01-21 08:53:15 +0530

    add pages

---
+++ client/src/pages/InsightsPage.tsx
@@ -0,0 +1,26 @@
+import Chat from "../components/Chat";
+import Insights from "../components/Insights";
+// import { useRecall } from "../contexts/RecallProvider";
+
+const InsightsPage = () => {
+  //   const { fileNames, transcriptionLanguage } = useAudio();
+
+  return (
+    <main className="min-h-screen overflow-hidden">
+      <div className="container mx-auto h-screen px-8 py-8">
+        <div className="flex h-full justify-between">
+          <Chat />
+          <Insights />
+        </div>
+        {/* {fileNames.map((filename) => (
+          <div className="text-white" key={filename}>
+            {filename}
+          </div>
+        ))}
+        {transcriptionLanguage} */}
+      </div>
+    </main>
+  );
+};
+
+export default InsightsPage;
